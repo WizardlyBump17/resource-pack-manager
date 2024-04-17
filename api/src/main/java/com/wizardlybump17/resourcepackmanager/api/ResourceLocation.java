52d2f47720552201fc2e967c13fa1a41559af409
WizardlyBump17 <davicraft130@gmail.com>
2024-04-16 23:06:21 -0300

    added the ResourceLocation.font(String, String) method

--- api/src/main/java/com/wizardlybump17/resourcepackmanager/api/ResourceLocation.java
+++ api/src/main/java/com/wizardlybump17/resourcepackmanager/api/ResourceLocation.java
@@ -7,4 +7,8 @@ public record ResourceLocation(@NonNull String namespace, @NonNull String path) 
     public static @NonNull ResourceLocation textures(@NonNull String namespace, @NonNull String path) {
         return new ResourceLocation(namespace, "textures/" + path);
     }
+
+    public static @NonNull ResourceLocation font(@NonNull String namespace, @NonNull String path) {
+        return new ResourceLocation(namespace, "font/" + path);
+    }
 }
