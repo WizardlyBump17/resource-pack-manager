cb26bc86004e2fd9c94cecae58e37407cd52eb2a
WizardlyBump17 <davicraft130@gmail.com>
2024-04-16 22:04:06 -0300

    added the ResourceLocation.textures(String, String) method

--- api/src/main/java/com/wizardlybump17/resourcepackmanager/api/ResourceLocation.java
+++ api/src/main/java/com/wizardlybump17/resourcepackmanager/api/ResourceLocation.java
@@ -3,4 +3,8 @@ package com.wizardlybump17.resourcepackmanager.api;
 import lombok.NonNull;
 
 public record ResourceLocation(@NonNull String namespace, @NonNull String path) {
+
+    public static @NonNull ResourceLocation textures(@NonNull String namespace, @NonNull String path) {
+        return new ResourceLocation(namespace, "textures/" + path);
+    }
 }
