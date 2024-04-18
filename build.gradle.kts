fa2ebccd987721c5f3bd0d8a24d6df61c0497bc9
WizardlyBump17 <davicraft130@gmail.com>
2024-04-17 22:14:30 -0300

    fixed the javadoc generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("java")
+    id("io.freefair.lombok") version "8.6"
 }
 
 allprojects {
     apply(plugin = "java")
     apply(plugin = "com.github.johnrengelman.shadow")
+    apply(plugin = "io.freefair.lombok")
 
     group = "com.wizardlybump17.resource-pack-manager"
     version = "0.0.1"
@@ -19,6 +21,15 @@ allprojects {
         compileJava {
             options.encoding = Charsets.UTF_8.name()
             options.release.set(18)
+        }
+
+        javadoc {
+            dependsOn(delombok)
+            options {
+                this as StandardJavadocDocletOptions
+                addBooleanOption("Xdoclint:none", true)
+                addStringOption("Xmaxwarns", "1")
+            }
         }
     }
 }
