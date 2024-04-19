f8d7602ba44c9f3fa9910c60adc3b7524e1dcc76
WizardlyBump17 <davicraft130@gmail.com>
2024-04-19 19:21:04 -0300

    reading the JsonParser correctly

--- api/src/main/java/com/wizardlybump17/resourcepackmanager/api/deserializer/font/provider/SpaceProviderDeserializer.java
+++ api/src/main/java/com/wizardlybump17/resourcepackmanager/api/deserializer/font/provider/SpaceProviderDeserializer.java
@@ -3,7 +3,9 @@ package com.wizardlybump17.resourcepackmanager.api.deserializer.font.provider;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.wizardlybump17.resourcepackmanager.api.resource.font.provider.SpaceProvider;
+import com.wizardlybump17.resourcepackmanager.api.util.DeserializationUtil;
 
 import java.io.IOException;
 import java.util.Map;
@@ -12,6 +14,7 @@ public class SpaceProviderDeserializer extends JsonDeserializer<SpaceProvider> {
 
     @Override
     public SpaceProvider deserialize(JsonParser parser, DeserializationContext context) throws IOException {
-        return new SpaceProvider(parser.readValueAs(Map.class));
+        JsonNode node = parser.getCodec().readTree(parser);
+        return new SpaceProvider(DeserializationUtil.getValue(parser, node, context, "advances", Map.class));
     }
 }
