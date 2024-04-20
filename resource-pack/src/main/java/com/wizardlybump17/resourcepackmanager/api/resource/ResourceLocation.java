920862cf14167b85152c21d032dcfee9ddeed10e
WizardlyBump17 <davicraft130@gmail.com>
2024-04-20 14:42:29 -0300

    added the ResourceLocation.fromString(String) method

--- resource-pack/src/main/java/com/wizardlybump17/resourcepackmanager/api/resource/ResourceLocation.java
+++ resource-pack/src/main/java/com/wizardlybump17/resourcepackmanager/api/resource/ResourceLocation.java
@@ -1,6 +1,7 @@
 package com.wizardlybump17.resourcepackmanager.api.resource;
 
 import lombok.NonNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * <p>
@@ -14,6 +15,13 @@ public record ResourceLocation(@NonNull String namespace, @NonNull String path) 
 
     public @NonNull String fullPath() {
         return "assets/" + namespace + "/" + path;
+    }
+
+    public static @Nullable ResourceLocation fromString(@NonNull String string) {
+        String[] split = string.split(":");
+        if (split.length > 2)
+            return null;
+        return new ResourceLocation(split.length == 1 ? "minecraft" : split[0], split.length == 1 ? split[0] : split[1]);
     }
 
     public static @NonNull ResourceLocation textures(@NonNull String namespace, @NonNull String path) {
