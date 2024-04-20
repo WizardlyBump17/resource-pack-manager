be52c26c2bdd1c767aa21b71bca7f77d9e978560
WizardlyBump17 <davicraft130@gmail.com>
2024-04-20 15:16:35 -0300

    added the Font test

--- api/src/test/java/com/wizardlybump17/resourcepackmanager/test/FontTest.java
+++ api/src/test/java/com/wizardlybump17/resourcepackmanager/test/FontTest.java
@@ -4,15 +4,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.wizardlybump17.resourcepackmanager.api.deserializer.font.provider.*;
 import com.wizardlybump17.resourcepackmanager.api.resource.ResourceLocation;
+import com.wizardlybump17.resourcepackmanager.api.resource.font.Font;
 import com.wizardlybump17.resourcepackmanager.api.resource.font.provider.*;
+import com.wizardlybump17.resourcepackmanager.api.resource.registry.FontProviderTypesRegistry;
 import com.wizardlybump17.resourcepackmanager.api.serializer.font.provider.*;
 import com.wizardlybump17.resourcepackmanager.test.util.SerializationUtil;
 import lombok.NonNull;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 class FontTest {
 
@@ -104,5 +108,62 @@ class FontTest {
                 )
         );
         SerializationUtil.testSerialization(MAPPER, provider);
+    }
+
+    @Test
+    void testFontSerialization() throws IOException {
+        FontProviderTypesRegistry.INSTANCE.register(BitmapProvider.TYPE, BitmapProvider.class);
+        FontProviderTypesRegistry.INSTANCE.register(LegacyUnicodeProvider.TYPE, LegacyUnicodeProvider.class);
+        FontProviderTypesRegistry.INSTANCE.register(ReferenceProvider.TYPE, ReferenceProvider.class);
+        FontProviderTypesRegistry.INSTANCE.register(SpaceProvider.TYPE, SpaceProvider.class);
+        FontProviderTypesRegistry.INSTANCE.register(TtfProvider.TYPE, TtfProvider.class);
+        FontProviderTypesRegistry.INSTANCE.register(UnihexProvider.TYPE, UnihexProvider.class);
+
+        List<FontProvider> possibleProviders = List.of(
+                new BitmapProvider(
+                        new ResourceLocation("test", "test.png"),
+                        8,
+                        6,
+                        List.of(
+                                "abc",
+                                "def"
+                        )
+                ),
+                new LegacyUnicodeProvider(
+                        new ResourceLocation("test", "test.bin"),
+                        new ResourceLocation("test", "test.png")
+                ),
+                new ReferenceProvider(new ResourceLocation("test", "test")),
+                new SpaceProvider(
+                        Map.of(
+                                'a', 1f,
+                                'b', 2f,
+                                'c', 3f
+                        )
+                ),
+                new TtfProvider(
+                        new ResourceLocation("test", "test.ttf"),
+                        List.of(1f, 2f),
+                        6,
+                        3,
+                        List.of('a', 'b', 'c')
+                ),
+                new UnihexProvider(
+                        new ResourceLocation("test", "test.hex"),
+                        List.of(
+                                new UnihexProvider.SizeOverride('a', 'c', 1, 2),
+                                new UnihexProvider.SizeOverride('d', 'f', 3, 4)
+                        )
+                )
+        );
+
+        int amount = 100;
+        List<FontProvider> providers = new ArrayList<>(amount);
+        Random random = new Random(0);
+        for (int i = 0; i < amount; i++)
+            providers.add(possibleProviders.get(random.nextInt(possibleProviders.size())));
+
+        Font font = new Font(providers);
+        SerializationUtil.testSerialization(MAPPER, font);
     }
 }
