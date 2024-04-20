435bbcc53bfc8c2bb39ee4fffeed1a28217e56b2
WizardlyBump17 <davicraft130@gmail.com>
2024-04-20 12:18:23 -0300

    using implementation instead

--- resource-pack/build.gradle.kts
+++ resource-pack/build.gradle.kts
@@ -2,7 +2,7 @@ val lombok = "1.18.30"
 val jetbrainsAnnotations = "24.1.0"
 
 dependencies {
-    compileOnly("org.projectlombok:lombok:${lombok}")
-    compileOnly("org.jetbrains:annotations:${jetbrainsAnnotations}")
+    implementation("org.projectlombok:lombok:${lombok}")
+    implementation("org.jetbrains:annotations:${jetbrainsAnnotations}")
     annotationProcessor("org.projectlombok:lombok:${lombok}")
 }
