d613fbbd91db890eee2b7bebb8e166d67b08aaed
WizardlyBump17 <davicraft130@gmail.com>
2024-04-20 12:20:49 -0300

    improved the dependency declaration

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -4,10 +4,12 @@ val paper = "1.20.4-R0.1-20240329.175742-144"
 val jackson = "2.17.0"
 
 dependencies {
-    implementation("org.projectlombok:lombok:${lombok}")
+    compileOnly("org.projectlombok:lombok:${lombok}")
     implementation("org.jetbrains:annotations:${jetbrainsAnnotations}")
     annotationProcessor("org.projectlombok:lombok:${lombok}")
     implementation(project(":resource-pack"))
     implementation("com.fasterxml.jackson.core:jackson-databind:${jackson}")
-//    testAnnotationProcessor("org.projectlombok:lombok:${lombok}")
+
+    testCompileOnly("org.projectlombok:lombok:${lombok}")
+    testAnnotationProcessor("org.projectlombok:lombok:${lombok}")
 }
